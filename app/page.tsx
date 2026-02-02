4681ede355979b09d4f6e461ce18bf702afc355b
Deeksha Singh <deekshasin1111@gmail.com>
2026-02-02 20:09:24 +0530

    Initial commit: setting up Next.js project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+
+  console.log("Hahaha")
+  return (
+    <div>
+      <div className="text-5xl underline">Welcome to Shasha's Project</div>
+    </div>
+  )
+}
+
+export default Home
